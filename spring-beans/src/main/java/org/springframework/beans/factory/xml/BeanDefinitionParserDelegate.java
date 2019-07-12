eb8d4d4dccfcc83c739d2c1df8f25b69340815a4
siming.liu <siming.liu@ttpai.cn>
2019-07-12 10:16:59 +0800

    get

--- spring-beans/src/main/java/org/springframework/beans/factory/xml/BeanDefinitionParserDelegate.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/xml/BeanDefinitionParserDelegate.java
@@ -772,16 +772,50 @@ public class BeanDefinitionParserDelegate {
 				parentName, className, this.readerContext.getBeanClassLoader());
 	}
 
+	/**
+	 *
+	 * 解析过程较为简单，获取相应的 key - value 构建 BeanMetadataAttribute 对象
+	 *
+	 * 然后调用 BeanMetadataAttributeAccessor#addMetadataAttribute(BeanMetadataAttribute) 方法
+	 *
+	 * 添加 BeanMetadataAttribute 加入到 AbstractBeanDefinition 中。
+	 *
+	 *  友情提示：
+	 *
+	 *	AbstractBeanDefinition 继承 BeanMetadataAttributeAccessor 类
+	 *  BeanMetadataAttributeAccessor 继承 AttributeAccessorSupport 类。
+	 *
+	 */
 	public void parseMetaElements(Element ele, BeanMetadataAttributeAccessor attributeAccessor) {
 		NodeList nl = ele.getChildNodes();
+		/**
+		 *
+		 * 遍历子节点
+		 *
+		 */
 		for (int i = 0; i < nl.getLength(); i++) {
 			Node node = nl.item(i);
+			/**
+			 *
+			 * <meta key="special-data" value="sprecial stragey" />
+			 *
+			 */
 			if (isCandidateElement(node) && nodeNameEquals(node, META_ELEMENT)) {
 				Element metaElement = (Element) node;
 				String key = metaElement.getAttribute(KEY_ATTRIBUTE);
 				String value = metaElement.getAttribute(VALUE_ATTRIBUTE);
+				/**
+				 *
+				 * 创建 BeanMetadataAttribute 对象
+				 *
+				 */
 				BeanMetadataAttribute attribute = new BeanMetadataAttribute(key, value);
 				attribute.setSource(extractSource(metaElement));
+				/**
+				 *
+				 * 添加到 BeanMetadataAttributeAccessor 中
+				 *
+				 */
 				attributeAccessor.addMetadataAttribute(attribute);
 			}
 		}
@@ -851,17 +885,38 @@ public class BeanDefinitionParserDelegate {
 
 	/**
 	 * Parse lookup-override sub-elements of the given bean element.
+	 *
+	 * 解析过程和 meta 子元素没有多大区别，同样是解析 methodName、beanRef 构造一个 LookupOverride 对象
+	 *
+	 * 然后记录到 AbstractBeanDefinition 中的 methodOverrides 属性中。
+	 *
+	 * 在实例化 Bean 的时候，再详细阐述具体的实现过程，这里仅仅只是一个标记作用。
 	 */
 	public void parseLookupOverrideSubElements(Element beanEle, MethodOverrides overrides) {
 		NodeList nl = beanEle.getChildNodes();
+		/**
+		 *
+		 * 遍历子节点
+		 *
+		 */
 		for (int i = 0; i < nl.getLength(); i++) {
 			Node node = nl.item(i);
 			if (isCandidateElement(node) && nodeNameEquals(node, LOOKUP_METHOD_ELEMENT)) {
 				Element ele = (Element) node;
 				String methodName = ele.getAttribute(NAME_ATTRIBUTE);
 				String beanRef = ele.getAttribute(BEAN_ELEMENT);
+				/**
+				 *
+				 * 创建 LookupOverride 对象
+				 *
+				 */
 				LookupOverride override = new LookupOverride(methodName, beanRef);
 				override.setSource(extractSource(ele));
+				/**
+				 *
+				 * 添加到 MethodOverrides 中
+				 *
+				 */
 				overrides.addOverride(override);
 			}
 		}
